0c5285446476b117d819f9bd01fa20ab951d154a
Karthik Chakkalakal <kchakkal@andrew.cmu.edu>
2025-11-26 08:28:27 -0500

    Clean up script fix.

--- infra/cleanup_script.sh
+++ infra/cleanup_script.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-docker compose down -v # Stop & remove containers/volumes
+# This script will release the resources used in the project without affecting other services.
+# Hopefully it helps =D.
 
-vagrant destroy -f # Destroy Vagrant VM
+docker compose down -v || true # Stop & remove containers/volumes
+
+vagrant destroy || true # Destroy Vagrant VMs
 rm -rf .vagrant # Remove local state of Vagrant
 
 vagrant box remove hashicorp-education/ubuntu-24-04 # Remove Vagrant boxes
