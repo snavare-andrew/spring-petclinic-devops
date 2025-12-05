1f7ab492893771e66511df1f39801c06261a6650
Karthik Chakkalakal <kchakkal@andrew.cmu.edu>
2025-12-05 14:50:53 -0500

    Naming fixes (KC)

--- infra/vm_provision.sh
+++ infra/vm_provision.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Script to setup OpenJDK 21 on the newly created prod VM to run the application
+# Script to setup OpenJDK 21 on the newly created VM to run the application
 
 sudo apt-get update
 sudo apt-get install -y openjdk-21-jre-headless
